7eefc9869e683aa5778ec358d46ce43da2961166
maciej88 <mkrywult88@gmail.com>
2023-07-09 18:01:05 +0200

    alembic create table movies

---
+++ db_migrations/versions/7c2c122104aa_create_table_movies.py
@@ -0,0 +1,31 @@
+"""create_table_movies
+
+Revision ID: 7c2c122104aa
+Revises: 
+Create Date: 2023-07-09 17:44:15.063772
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7c2c122104aa'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute("""
+    create table movies(
+        movie_id serial primary key,
+        title text
+    )
+    """)
+
+
+def downgrade() -> None:
+    op.execute("""
+    drop table movies
+    """)
