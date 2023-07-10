57073eeabc665006b795258e2a80727794df77fb
maciej88 <mkrywult88@gmail.com>
2023-07-10 18:06:09 +0200

    add number to migration

--- db_migrations/versions/7c2c122104aa_1_create_table_movies.py
+++ db_migrations/versions/7c2c122104aa_1_create_table_movies.py
@@ -1,11 +1,14 @@
-"""create_table_movies
+"""1_create_table_movies
 
 Revision ID: 7c2c122104aa
 Revises: 
 Create Date: 2023-07-09 17:44:15.063772
 
 """
+import os
+
 from alembic import op
+from dotenv import load_dotenv
 import sqlalchemy as sa
 
 
@@ -14,7 +17,6 @@ revision = '7c2c122104aa'
 down_revision = None
 branch_labels = None
 depends_on = None
-
 
 def upgrade() -> None:
     op.execute("""
